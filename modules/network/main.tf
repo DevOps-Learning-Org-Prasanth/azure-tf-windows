20c32e3c6f5a76f902b792fe26174451892de445
Prasanth-518 <prasanthvg3@gmail.com>
2021-09-28 19:51:55 +0530

    Resolved vnet attributes call issue

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -2,11 +2,11 @@
 
 resource "azurerm_virtual_network" "main" {
   for_each            = { for subnet in var.subnet_space : subnet.name => subnet.address_prefix }
-  name                = var.vnet_space[name]
+  name                = var.vnet_space.name
   resource_group_name = var.resource_group_name
   location            = var.location
 
-  address_space = [var.vnet_space[address_space]]
+  address_space = [var.vnet_space.address_space]
 
   subnets {
     name           = each.value.name
