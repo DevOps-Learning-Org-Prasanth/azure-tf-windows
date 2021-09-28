3ea82c3e961c03f772fec50ff0928062b8f3866a
Prasanth-518 <prasanthvg3@gmail.com>
2021-09-28 20:03:11 +0530

    Testing for-each in subnet block

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,7 +1,6 @@
 # vnet
 
 resource "azurerm_virtual_network" "main" {
-  for_each            = { for subnet in var.subnet_space : subnet.name => subnet.address_prefix }
   name                = var.vnet_space.name
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -9,6 +8,7 @@ resource "azurerm_virtual_network" "main" {
   address_space = [var.vnet_space.address_space]
 
   subnet {
+    for_each       = { for subnet in var.subnet_space : subnet.name => subnet.address_prefix }
     name           = each.key
     address_prefix = each.value
   }
