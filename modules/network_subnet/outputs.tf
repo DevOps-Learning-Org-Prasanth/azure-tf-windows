016cc773dd77daa88e01bbcc36cf6726416db90d
Prasanth-518 <prasanthvg3@gmail.com>
2021-09-29 08:21:27 +0530

    Resolving issues with subnet module

--- modules/network_subnet/outputs.tf
+++ modules/network_subnet/outputs.tf
@@ -1,7 +1,7 @@
 output "subnets" {
   value = { for subnet in var.subnets :
     (subnet.name) => {
-      id   = azurerm_subnet.main[subnet.name]
+      id   = azurerm_subnet.main[subnet.name].id
       cidr = subnet.cidr
     }
   }
