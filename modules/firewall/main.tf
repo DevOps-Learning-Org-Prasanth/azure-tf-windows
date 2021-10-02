9a073f00cec5683aea80bb73311486eb0566be98
pg185155 <pg185155@ncr.com>
2021-10-02 16:12:09 +0530

    Adding nsg rule for rdp

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -16,8 +16,23 @@ resource "azurerm_network_security_rule" "allow_rdp" {
   source_address_prefix = "*"
   source_port_range = "3389"
   destination_address_prefix = "*"
-  destination_port_range = "*"
+  destination_port_range = "3389"
 } 
+
+resource "azurerm_network_security_rule" "allow_rdp_outbound" {
+  name = "Allow RDP inbound"
+  resource_group_name = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.main.name
+  
+  priority = 101
+  direction = "Outbound"
+  access = "Allow"
+  protocol  = "*"
+  source_address_prefix = "*"
+  source_port_range = "3389"
+  destination_address_prefix = "*"
+  destination_port_range = "3389"
+}
 
 resource "azurerm_subnet_network_security_group_association" "main" {
   network_security_group_id = azurerm_network_security_group.main.id
