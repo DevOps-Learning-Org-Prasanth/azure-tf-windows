36961da8b2be7d296b9223a69c64d7c64afb3876
pg185155 <pg185155@ncr.com>
2021-10-02 16:00:10 +0530

    Updated vm extension

---
+++ modules/firewall/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_network_security_group" "main" {
+  name = "Windows-VM-NSG"
+  resource_group_name = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_network_security_rule" "allow_rdp" {
+  name = "Allow RDP inbound"
+  resource_group_name = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.main.name
+  
+  priority = 101
+  direction = "Inbound"
+  access = "Allow"
+  protocol  = "*"
+  source_address_prefix = "*"
+  source_port_range = "3389"
+  destination_address_prefix = "*"
+  destination_port_range = "*"
+} 
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  network_security_group_id = azurerm_network_security_group.main.id
+  subnet_id = var.subnet_id
+}
