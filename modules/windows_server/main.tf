0160fc0eaab0b4985e6b5192cc9a89cc32a6357d
Prasanth <51731588+Prasanth-518@users.noreply.github.com>
2024-01-21 12:09:22 +0530

    Update main.tf

--- modules/windows_server/main.tf
+++ modules/windows_server/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_windows_virtual_machine" "main" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    sku       = "2022-Datacenter"
     version   = "latest"
   }
 }
